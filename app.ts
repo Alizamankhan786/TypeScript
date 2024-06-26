8925e12487b6d8daf9fe52732579fdf201ff8493
Alizamankhan786 <az030366@gmail.com>
2024-06-26 00:38:33 -0700

    index file created

---
+++ app.ts
@@ -0,0 +1,16 @@
+// TS Compiler P
+// Type Annotations
+// Type Inference
+// Type Aliases
+// Union Types
+// Interfaces
+// Type Aliases vs Interface
+// Generics
+// Enums
+// Tuples
+// Type Guards
+
+
+                                       // TYPE ANNOTATION STARTED
+
+
